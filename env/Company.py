a9dd16e8a35a0f51a1077b4c44b7d817ab51fc37
filip-herceg <pvt.filip.herceg@gmail.com>
2022-11-30 16:09:01 +0100

    Updated many things

---
+++ env/Company.py
@@ -0,0 +1,10 @@
+from env.Possession import Possession
+
+
+class Company(Possession):
+    # Need to contain/do:
+    # Can produce products
+    # Can consume material
+    # Can hire/fire Subjects
+    # MUST pay Employees
+    pass
--- subjects/Human.py
+++ subjects/Human.py
@@ -1,6 +1,7 @@
-import math
 import random
+
 from sympy import *
+
 x, y = symbols('x y')
 
 import subjects.Subject
@@ -10,45 +11,64 @@ class Human(subjects.Subject.Subject):
     name = str
     surname = str
 
-    #Personality start
+    # Personality start
     risk = float
     friendliness = float
     greed = float
-    #Personality end
+    # Personality end
 
     money = 100
     health = 1
     alive = "true"
 
-    def __init__(self):         #Constructor of class Human
+    def __init__(self):  # Constructor of class Human
         self.default_stats()
+        self.get_info()
 
-
-    def get_info(self): #UNFINISHED
+    def get_info(self):
         print("Risk: ", self.risk)
+        print("Friendliness: ", self.friendliness)
+        print("Greed: ", self.greed)
 
     def default_stats(self):
         self.risk = self.__mutation(0.5)
         self.friendliness = self.__mutation(0.5)
         self.greed = self.__mutation(0.5)
 
-    def __mutation(self, feature):                      #Feature (m) is the x value where func1 and func2 cross
-        #Uses sympy lirbary to calculate
+    def __mutation(self, feature):  # Feature (m) is the x value where func1 and func2 cross
+        # Uses sympy lirbary to calculate
 
-        funca = x ** 2 / feature ** 2                   #First functinon: f(x)
-        integrala = Integral(funca, (x, 0, feature))    #Integral of first Function
-        randomnum = random.random()                     #Random number between 0 and 1
+        # First functinon: f(x)
+        funca = x ** 2 / feature ** 2
 
-        if randomnum <= feature:    #Is this compare even correct?
-            print("randomnum: ", randomnum)             #DELETE ME
-            print("integrala: ", integrala.doit())      #doit() means "return decimal instead of formula" DELETE ME
-            ###CONTINUE
+        # Integral of first Function
+        integrala = Integral(funca, (x, 0, feature))
 
-        if randomnum > feature:
-            funcb = (x-1)**2 / (1-feature)**2
-            integralb = Integral(funcb, (x, feature, 1))
-            ###CONTINUE
+        # Second function: g(x)
+        funcb = (x - 1) ** 2 / (1 - feature) ** 2
 
+        # Integral of second Function
+        integralb = Integral(funcb, (x, feature, 1))
+
+        # Sum of both integrals
+        sumintegral = integrala + integralb
+
+        # Random number between 0 and 1
+        randomnum = random.random()
+
+        # Part of sum of integrals (0 % - 100%)
+        partintegral = randomnum * sumintegral
+
+        if partintegral <= integrala:
+            resultfunc = ((x ** 3) / (3 * (feature ** 2))) - partintegral
+            result = nsolve(resultfunc, x)
+            return result
+
+        else:
+            resultfunc = (feature / 3) + (((x ** 3 / 3) - x ** 2 + x) / ((1 - feature) ** 2)) - (
+                        ((feature ** 3 / 3) - feature ** 2 + feature) / ((1 - feature) ** 2)) - partintegral
+            result = nsolve(resultfunc, x)
+            return result
 
     def check_death(self):
         if (self.health == 0):
