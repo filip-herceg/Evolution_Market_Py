d24608b1b7577c4dcf3bc83f3efbaf3aaeb8d994
Filip <pvt.filip.herceg@gmail.com>
2022-12-09 19:35:20 +0100

    Updated Subjects.py: buy() and sell()

--- subjects/Subject.py
+++ subjects/Subject.py
@@ -10,21 +10,40 @@ class Subject:
     possessions: int
 
 
-    def __init__(self):
-        isBank = self.__check_bank()
+    def __init__(self, name: str, personality: Dict[str, float]):
+        self.name = name
+        self.personality = personality
 
     def __check_bank(self):  # works!
         if type(self).__name__ == "Bank":
             return "true"
         return "false"
 
-    # This method allows the subject to purchase items from the market, using its money and possessions.
-    def buy(self):
-        pass
+        # This method allows the subject to purchase items from the market, using its money and possessions.
+        def buy(self, item: Item):
+            # Use the subject's personality traits to determine how much they are willing to pay for the item
+            price = item.value * (1 + self.personality["greed"] - self.personality["frugality"])
 
-    # This method allows the subject to sell items to the market, in exchange for money or other possessions.
-    def sell(self):
-        pass
+            # If the subject has enough money to buy the item, make the purchase
+            if self.money >= price:
+                self.possessions.append(item)
+                self.money -= price
+                print(f"{self.name} bought {item.name} for ${price:.2f}.")
+            else:
+                print(f"{self.name} doesn't have enough money to buy {item.name}.")
+
+        # This method allows the subject to sell items to the market, in exchange for money or other possessions.
+        def sell(self, item: Item):
+            # Use the subject's personality traits to determine how much they are willing to sell the item for
+            price = item.value * (1 - self.personality["greed"] + self.personality["frugality"])
+
+            # If the subject has the item to sell, make the sale
+            if item in self.possessions:
+                self.possessions.remove(item)
+                self.money += price
+                print(f"{self.name} sold {item.name} for ${price:.2f}.")
+            else:
+                print(f"{self.name} doesn't have {item.name} to sell.")
 
     # This method allows the subject to interact with other subjects, such as by trading items or exchanging
     # information.
